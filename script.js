f1425ff0c635d4da85c20e6ab0312f422f32cb86
Atulgupta0403 <atulgupta0403@gmail.com>
2024-02-14 02:49:44 +0530

    initiazing golf site

---
+++ script.js
@@ -0,0 +1,44 @@
+var cursor = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+document.addEventListener("mouseover" , (dets) => {
+    // console.log(dets.x,dets.y);
+    cursor.style.left = dets.x+"px";
+    cursor.style.top = dets.y+"px";
+
+})
+
+document.addEventListener("mouseover",(dets)=>{
+    blur.style.top = dets.y - 200 +"px";
+    blur.style.left = dets.x -200 +"px";
+})
+
+gsap.to("#nav",{
+    backgroundColor:"black",
+    // duration:0.5,
+    // delay:1
+    height:"110px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        // scrub:true
+        scrub:2
+    }
+})
+
+gsap.to(".main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:true
+    }
+    
+})
+
+
